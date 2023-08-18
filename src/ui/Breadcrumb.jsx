a1712838cc4b99ab5335d4a16a695529c79ccf5a
tkahmedkamal <tkahmedkamal@hotmail.com>
2023-08-18 12:53:26 +0300

    Refactor breadcrumb component

--- src/ui/Breadcrumb.jsx
+++ src/ui/Breadcrumb.jsx
@@ -1,44 +1,54 @@
-import { Link } from 'react-router-dom';
+import { Fragment } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { BsDot } from 'react-icons/bs';
 
-const Breadcrumb = ({ prev, current, isPrev, prevLabel, prevLink }) => {
+const Breadcrumb = () => {
   const { t } = useTranslation();
+  const { pathname } = useLocation();
+
+  const crumbs = pathname
+    .split('/')
+    .map(crumb => crumb)
+    .filter(crumb => crumb !== '');
+  let url = '';
 
   return (
     <nav className='flex font-publicSans' aria-label='Breadcrumb'>
-      <ol className='inline-flex items-center space-x-1 md:space-x-3'>
-        <li className='inline-flex items-center'>
-          <Link
-            to='/'
-            className='inline-flex items-center text-sm text-gray hover:underline dark:text-dark-secondary-text'
-          >
-            {prev || t('sidebar.dashboard')}
-          </Link>
-        </li>
-        {isPrev && (
-          <li aria-current='page'>
-            <Link
-              to={`/${prevLink}`}
-              className='flex items-center text-sm text-gray hover:underline dark:text-dark-secondary-text'
-            >
-              <span className='text-xl text-gray/80'>
-                <BsDot />
-              </span>
-              {prevLabel}
-            </Link>
-          </li>
-        )}
-        <li aria-current='page'>
-          <div className='flex items-center'>
-            <span className='text-xl text-gray/80'>
-              <BsDot />
-            </span>
-            <span className='ml-1 text-sm text-gray/60 dark:text-dark-secondary-text/50 md:ml-2'>
-              {current}
-            </span>
-          </div>
-        </li>
+      <ol className='inline-flex items-center'>
+        {crumbs.map((crumb, index) => {
+          url += `/${crumb}`;
+          const firstIndex = index !== 0;
+          const lastIndex = index === crumbs.length - 1;
+
+          return (
+            <Fragment key={index}>
+              {!lastIndex && (
+                <li>
+                  <Link
+                    to={url}
+                    className='flex items-center text-sm capitalize text-gray hover:underline dark:text-dark-secondary-text'
+                  >
+                    {firstIndex && (
+                      <span className='text-xl text-gray/80'>
+                        <BsDot />
+                      </span>
+                    )}
+                    {t(`sidebar.${crumb}`)}
+                  </Link>
+                </li>
+              )}
+              {lastIndex && (
+                <p className='flex items-center text-sm capitalize text-gray/60 dark:text-dark-secondary-text/60'>
+                  <span className='text-xl text-gray/80'>
+                    <BsDot />
+                  </span>
+                  {t(`sidebar.${crumb}`)}
+                </p>
+              )}
+            </Fragment>
+          );
+        })}
       </ol>
     </nav>
   );
